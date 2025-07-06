7007f0f9167d19b0956ff209cb06a5f980b1c859
Darkhan <darkhanlukmankhakim@gmail.com>
2025-07-06 09:13:39 +0600

    [fix]

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,10 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { Home } from './pages/home/home';
 import { SearchPool } from './pages/search-pool/search-pool';
-
+import { HoverDirective } from './shared/directives/hover.directive';
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer, RouterOutlet],
+  imports: [Header, Footer, RouterOutlet,HoverDirective],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,10 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { HoverDirective } from '../../shared/directives/hover.directive';
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink,HoverDirective],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })

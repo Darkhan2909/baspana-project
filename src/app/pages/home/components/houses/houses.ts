8965897293c93df6cd32027c1e5a42b0943d6a1b
Darkhan <darkhanlukmankhakim@gmail.com>
2025-08-31 11:06:15 +0600

    [fix] to changes add destroy

--- src/app/pages/home/components/houses/houses.ts
+++ src/app/pages/home/components/houses/houses.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { DecimalPipe, CommonModule } from '@angular/common';
 import { debounceTime, distinctUntilChanged, startWith } from 'rxjs/operators';
 import * as HousesActions from '../../../../store/houses/houses.actions'; // Ensure this path is correct and the file exists
 import { selectAllHouses } from '../../../../store/houses/houses.selectors'; // Ensure this path is correct and the file exists
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-houses',
@@ -15,7 +16,7 @@ import { selectAllHouses } from '../../../../store/houses/houses.selectors'; // 
 })
 export class HousesComponent implements OnInit {
   private store = inject(Store);
-
+  private destroyRef = inject(DestroyRef); 
   houses$ = this.store.select(selectAllHouses);
   search = new FormControl('');
 
@@ -25,7 +26,7 @@ export class HousesComponent implements OnInit {
 
     // авто-поиск
     this.search.valueChanges
-      .pipe(startWith(''), debounceTime(300), distinctUntilChanged())
+      .pipe(startWith(''), debounceTime(300), distinctUntilChanged(), takeUntilDestroyed(this.destroyRef))
       .subscribe(q => {
         this.store.dispatch(HousesActions.loadHouses({ query: q || '' }));
       });

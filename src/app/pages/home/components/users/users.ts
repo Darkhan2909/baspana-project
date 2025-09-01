c97da8f1e929fa8e1a66d6871ac00c5ff54dc9da
Darkhan <darkhanlukmankhakim@gmail.com>
2025-09-01 08:56:20 +0600

    [fix] to changes add actions

--- src/app/pages/home/components/users/users.ts
+++ src/app/pages/home/components/users/users.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { UsersService } from '../../../../shared/Services/users';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-users',
+  standalone: true,
   imports: [],
   templateUrl: './users.html',
   styleUrl: './users.scss'
 })
 export class Users {
   users: any[] = [];
+  private destroyRef = inject(DestroyRef); 
   constructor(private usersService: UsersService) {}
   ngOnInit(): void {
     // вот тут идет subscribe 👇
     this.usersService.getUsers()
-    .pipe(takeUntilDestroyed())
+    .pipe(takeUntilDestroyed(this.destroyRef)) // автоматически отписываемся при уничтожении компонента
     .subscribe((data) => {
       this.users = data;
       console.log('Данные пришли:', data);
--- src/app/pages/programms/programms.ts
+++ src/app/pages/programms/programms.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { QuickLinks } from '../home/components/quick-links/quick-links';
+import { Users } from "../home/components/users/users";
 @Component({
   selector: 'app-programms',
-  imports: [QuickLinks],
+  standalone: true,
+  imports: [QuickLinks, Users],
   templateUrl: './programms.html',
   styleUrl: './programms.scss'
 })

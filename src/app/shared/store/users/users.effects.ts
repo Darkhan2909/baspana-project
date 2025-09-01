030e081d362c19c0925fdccdf3d9c6b3705c7777
Darkhan <darkhanlukmankhakim@gmail.com>
2025-09-01 10:16:13 +0600

    [fix]

--- src/app/shared/store/users/users.effects.ts
+++ src/app/shared/store/users/users.effects.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { UsersService } from '../../../shared/Services/users';
 import * as UsersActions from './users.actions';
@@ -6,6 +6,8 @@ import { catchError, map, mergeMap, of } from 'rxjs';
 
 @Injectable()
 export class UsersEffects {
+  actions$ = inject(Actions);
+  usersService = inject(UsersService);
   loadUsers$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UsersActions.loadUsers),
@@ -18,5 +20,4 @@ export class UsersEffects {
     )
   );
 
-  constructor(private actions$: Actions, private usersService: UsersService) {}
 }
